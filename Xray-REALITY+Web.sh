399f24a6f5d75cf43f7a5deab13d5c73b0f2f0b5
Btu2023V <119510270+Btu2023V@users.noreply.github.com>
2025-11-08 18:42:51 +0800

    Update Xray-REALITY+Web.sh

--- Xray-REALITY+Web.sh
+++ Xray-REALITY+Web.sh
@@ -17,8 +17,8 @@ unset nproc
 
 
 # 安装配置（nginx、openssl、PHP、cloudreve、xray的安装版本、位置等信息）
-nginx_version="nginx-1.26.2"
-openssl_version="openssl-openssl-3.5.0"
+nginx_version="nginx-1.28.0"
+openssl_version="openssl-openssl-3.6.0"
 nginx_prefix="/usr/local/nginx"
 nginx_config="${nginx_prefix}/conf.d/xray.conf"
 nginx_service="/etc/systemd/system/nginx.service"
